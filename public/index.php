78f7e8507266dd124b9067159847e6485647e205
Your Name <your@email.com>
2026-06-30 11:57:29 +0800

    Debug Laravel bootstrap step test

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+
 echo "STEP 1<br>";
 
 require __DIR__.'/../vendor/autoload.php';
@@ -9,4 +11,11 @@ echo "STEP 2<br>";
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 echo "STEP 3<br>";
-exit;
+
+$request = Request::capture();
+
+echo "STEP 4<br>";
+
+$app->handleRequest($request);
+
+echo "STEP 5<br>";
