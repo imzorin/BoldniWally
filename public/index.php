8530b60fdc299cdebe30dfc4399f21de95dfacd2
Your Name <your@email.com>
2026-06-30 11:43:04 +0800

    Debug Laravel bootstrap step test

--- public/index.php
+++ public/index.php
@@ -1,4 +1,12 @@
 <?php
 
-echo "INDEX WORKS";
+echo "STEP 1<br>";
+
+require __DIR__.'/../vendor/autoload.php';
+
+echo "STEP 2<br>";
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+echo "STEP 3<br>";
 exit;
